8bb1cb95ae2e2b6fb0908358bd745b5e909f0355
Simone Polo <simone@nanalab.com>
2018-07-11 17:02:56 +0200

    Initial commit

---
+++ src/main/kotlin/com/tfx/demo/app/MyApp.kt
@@ -0,0 +1,6 @@
+package com.tfx.demo.app
+
+import com.tfx.demo.view.MainView
+import tornadofx.App
+
+class MyApp: App(MainView::class, Styles::class)
---
+++ src/main/kotlin/com/tfx/demo/view/MainView.kt
@@ -0,0 +1,17 @@
+package com.tfx.demo.view
+
+import javafx.scene.control.Label
+import javafx.scene.layout.AnchorPane
+import tornadofx.*
+
+class MainView : View("Hello TornadoFX") {
+    override val root : AnchorPane by fxml("/ui/view/MainView.fxml")
+
+    private var i = 0
+    private val lblOutput : Label by fxid(/*"lblOutput"*/)
+
+    fun onBtnClicked() {
+        i ++
+        lblOutput.text("Value of i : $i")
+    }
+}
