657e1a68d4b64ad81b3e153989417f4c95c157d8
Simone Polo <simone@nanalab.com>
2018-07-16 19:20:04 +0200

    adding property with bind

--- src/main/kotlin/com/tfx/demo/view/MainView.kt
+++ src/main/kotlin/com/tfx/demo/view/MainView.kt
@@ -1,6 +1,7 @@
 package com.tfx.demo.view
 
 import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
 import javafx.scene.control.Label
 import javafx.scene.control.TextField
 import javafx.scene.layout.AnchorPane
@@ -12,15 +13,19 @@ class MainView : View("Hello TornadoFX") {
     private val m_lblOutput : Label by fxid("lblOutput")
     private val m_txtName : TextField by fxid("txtName")
     private val m_lblCounter : Label by fxid("lblCounter")
-    val counter = SimpleIntegerProperty()
+    //private val counter = SimpleIntegerProperty()
+    private val propLabel = SimpleStringProperty()
 
     init {
-        m_lblCounter.bind(counter)
+        //m_lblCounter.bind(counter)
+        m_lblCounter.bind(propLabel)
     }
 
     fun onBtnClicked() {
-        counter.value ++
+        //counter.value ++
         val name = m_txtName.text
-        m_lblOutput.text = "Value of i : ${counter.value} ($name)"
+        val message = "Value of i : ${counter.value} ($name)"
+        propLabel.value = message
+        m_lblOutput.text = message
     }
 }
